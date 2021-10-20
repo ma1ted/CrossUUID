e99d93724ed89b845f0e06ae86c1458bcb4ee24e
Malted <59726149+ma1ted@users.noreply.github.com>
2021-10-20 12:58:05 +0100

    :white_check_mark: Update tests

--- validate.test.ts
+++ validate.test.ts
@@ -1,14 +1,14 @@
 import { assert } from "https://deno.land/std@0.112.0/testing/asserts.ts";
 import { v4 } from "https://deno.land/std@0.112.0/uuid/mod.ts";
 
-import { CrossUUID } from "./v4uuid.ts";
+import { V4UUID } from "./v4uuid.ts";
 
 Deno.test("Validate generated UUID", () => {
   let passed = true;
   const count = 10_000;
 
   for (let i = 0; i < count; i++) {
-    const uuid: string = new CrossUUID().generate();
+    const uuid: string = new V4UUID().generate();
     const isValid: boolean = v4.validate(uuid);
     if (!isValid) {
       passed = false;
